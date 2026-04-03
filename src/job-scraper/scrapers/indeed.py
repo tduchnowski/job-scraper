92abff67ea093bd91f3cd2ea071d2a06776d0a1d
Tomasz Duchnowski <entidzow@gmail.com>
2026-04-03 14:54:08 +0200

    Update indeed list scraper to use Job model

--- src/job-scraper/scrapers/indeed.py
+++ src/job-scraper/scrapers/indeed.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import aiohttp
 from loguru import logger
+from models.job import Job
 
 class IndeedScraper:
     def __init__(self, session: aiohttp.ClientSession):
@@ -11,7 +12,7 @@ class IndeedScraper:
     async def scrape_job_list(self, query: str, location: str):
         logger.info(f"Scraping Indeed, query={query}, location={location}")
         resp = await self._fetch_job_list(query, location)
-        self._parse_job_list(resp)
+        jobs = self._parse_job_list(resp)
 
     async def _fetch_job_list(self, query: str, location: str) -> str:
         headers = {
@@ -30,21 +31,38 @@ class IndeedScraper:
         async with self._session.get(self._base_url, params=params, headers=headers) as response:
             return await response.text()
 
-    def _parse_job_list(self, html: str):
+    def _parse_job_list(self, html: str) -> list[Job]:
         soup = BeautifulSoup(html, "lxml")
         jobs = soup.find_all("div", {"class": "job_seen_beacon"})
+        result: list[Job] = []
+
         for job in jobs:
             title = job.find("h2", {"class": "jobTitle"})
-            title = title.get_text(strip=True) if title else "N/A"
+            title = title.get_text(strip=True) if title else None
             company = job.find("span", {"data-testid": "company-name"})
-            company = company.get_text(strip=True) if company else "N/A"
+            company = company.get_text(strip=True) if company else None
+            if not title or not company:
+                logger.warning("Missing title or company, skipping")
+                continue
             link = job.find("a", href=True)
-            jk = ""
-            job_url = ""
-            if link:
-                jk = link.get('data-jk', "")
-                job_url = f"{self._jobview_url}?jk={jk}" if jk else ""
-            logger.info(f"Job -- {title}, {company}, {jk}, {job_url}")
+            if not link:
+                logger.warning("Couldn't extract link information, skipping")
+                continue
+            jk = link.get('data-jk')
+            if not jk:
+                logger.warning(f"Couldn't extract job id")
+                continue
+            job_url = f"{self._jobview_url}?jk={jk}"
+
+            result.append(
+                Job(
+                    id=str(jk),
+                    title=title,
+                    company=company,
+                    url=job_url
+                )
+            )
+        return result
 
     def scrape_job_details(self, job_id: str):
         pass
